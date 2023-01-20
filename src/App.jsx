8c83aec0a7d3b22558360c4d29fc05785baaff61
Nesar Ahmad Patwary <nesarahmadpatwary@outlook.com>
2023-01-20 13:02:05 +0600

    tailwindcss added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
